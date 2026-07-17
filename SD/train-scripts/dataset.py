1a5556d05b2c1d06b190b12cb39ce8647de6f7c3
OneAndZero24 <oneandzero24@gmail.com>
2026-07-17 12:36:26 +0200

    .

--- SD/train-scripts/dataset.py
+++ SD/train-scripts/dataset.py
@@ -106,7 +106,7 @@ def setup_model(config, ckpt, device):
     if isinstance(config, (str, Path)):
         config = OmegaConf.load(config)
 
-    pl_sd = torch.load(ckpt, map_location="cpu")
+    pl_sd = torch.load(ckpt, map_location="cpu", weights_only=False)
     global_step = pl_sd["global_step"]
     sd = pl_sd["state_dict"]
     model = instantiate_from_config(config.model)
