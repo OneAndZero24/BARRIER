2bd3cc63b96f347520a61560322e608a98fb8a30
OneAndZero24 <oneandzero24@gmail.com>
2026-09-08 11:54:00 +0200

    .

--- DDPM/scripts/slurm_ablation_regions_boxes48.sh
+++ DDPM/scripts/slurm_ablation_regions_boxes48.sh
@@ -34,7 +34,7 @@
 
 # ---- Environment ----
 source ~/miniconda3/etc/profile.d/conda.sh
-conda activate salun-ddpm2
+conda activate /shared/results/common/miksa/envs/salun-ddpm2
 cd $HOME/InTAct-Unl/DDPM
 export PYTHONPATH=$PYTHONPATH:/home/miksa/InTAct-Unl/
 
